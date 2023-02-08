d6c8b0a26785912d9e666ca6608d39479a7084dc
Andrei Rat <andreirat@Andreis-MacBook-Pro-2.local>
2023-02-08 09:10:12 +0200

    chore: initial commit

---
+++ android/src/main/java/com/customwidget/CustomWidgetModule.java
@@ -0,0 +1,32 @@
+package com.customwidget;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.module.annotations.ReactModule;
+
+@ReactModule(name = CustomWidgetModule.NAME)
+public class CustomWidgetModule extends ReactContextBaseJavaModule {
+  public static final String NAME = "CustomWidget";
+
+  public CustomWidgetModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+  }
+
+  @Override
+  @NonNull
+  public String getName() {
+    return NAME;
+  }
+
+
+  // Example method
+  // See https://reactnative.dev/docs/native-modules-android
+  @ReactMethod
+  public void multiply(double a, double b, Promise promise) {
+    promise.resolve(a * b);
+  }
+}
---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  CustomWidgetExample
+//
+
+import Foundation
